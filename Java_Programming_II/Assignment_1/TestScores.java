d321cca4c0673b68b7e7e673ad8616556ad7cd5d
caxter99 <caxter99@gmail.com>
2020-09-10 13:44:01 -0400

    class now throws an exceptions properly

--- Java_Programming_II/Assignment_1/TestScores.java
+++ Java_Programming_II/Assignment_1/TestScores.java
@@ -2,11 +2,34 @@ import java.util.stream.*;
 
 public class TestScores
 {
+   final int MAX_SCORE = 100;
+   final int MIN_SCORE = 0;
    int[] testScores;
       
    public TestScores(int[] testScoresInit)
    {
-      testScores = testScoresInit;
+      if (containsValidScores(testScoresInit)) {
+         testScores = testScoresInit;
+      }
+      else {
+         testScores = new int[0];
+      }
+   }
+   
+   private boolean containsValidScores(int[] scores)
+   {
+      for (int x = 0; x < scores.length; x++)
+      {
+         if (scores[x] < MIN_SCORE)
+         {
+            throw new IllegalArgumentException("Error: Scores cannot be less than " + MIN_SCORE);
+         }
+         else if (scores[x] > MAX_SCORE) {
+            throw new IllegalArgumentException("Error: Scores cannot be greater than " + MAX_SCORE);
+         }
+      }
+      
+      return true;
    }
    
    public double getAverageTestScore()
@@ -35,10 +58,10 @@ public class TestScores
       int[] validTestScores = testScoreGenerator.generateValidTestScores(10);
       TestScores validScores = new TestScores(validTestScores);
       
-      int[] invalidTestScores = testScoreGenerator.generateInvalidTestScores(10);
-      TestScores invalidScores = new TestScores(invalidTestScores);
-      
       validScores.displayTestScores();
       System.out.println("The average of the valid test scores is: " + validScores.getAverageTestScore());
+      
+      int[] invalidTestScores = testScoreGenerator.generateInvalidTestScores(10);
+      TestScores invalidScores = new TestScores(invalidTestScores);
    }
 }
