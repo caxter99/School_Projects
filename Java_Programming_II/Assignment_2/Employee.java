909e85d6f7811aa69bca26c96898940a55fca21e
caxter99 <caxter99@gmail.com>
2020-10-13 14:09:35 -0400

    got the addition of employees and the map to work

--- Java_Programming_II/Assignment_2/Employee.java
+++ Java_Programming_II/Assignment_2/Employee.java
@@ -1,1 +1,16 @@
-// blah
+public class Employee
+{
+   int id;
+   String name;
+   
+   public Employee(int id, String name)
+   {
+      this.id = id;
+      this.name = name;
+   }
+   
+   public String toString()
+   {
+      return "Employee Name: " + name + "\nEmployee ID: " + id;
+   }
+}
--- Java_Programming_II/Assignment_2/EmployeeMap.java
+++ Java_Programming_II/Assignment_2/EmployeeMap.java
@@ -1,1 +1,26 @@
-// blah
+import java.util.*;
+
+public class EmployeeMap
+{
+   Map<Integer, Employee> employeeMap;
+   
+   public EmployeeMap()
+   {
+      employeeMap = new HashMap<Integer, Employee>();
+   }
+   
+   public void addEmployee(Employee employee)
+   {
+      employeeMap.put(employee.id, employee);
+   }
+   
+   public String toString()
+   {
+      String mapString = "Displaying all employees in the map:\n";
+      for (Map.Entry<Integer, Employee> mapElement : employeeMap.entrySet())
+      {
+         mapString += "ID: " + mapElement.getKey() + ", Name: " + mapElement.getValue().name + "\n";
+      } 
+      return mapString;
+   }
+}
