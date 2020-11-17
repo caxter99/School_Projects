bb11ceb9ab1b36f7f3d7bd122ad753a9de501433
caxter99 <caxter99@gmail.com>
2020-11-17 17:50:53 -0500

    got the general idea down, have even handlers and radio buttons in place

--- Java_Programming_II/Assignment_3/CellSolutions.java
+++ Java_Programming_II/Assignment_3/CellSolutions.java
@@ -5,11 +5,17 @@ import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.control.RadioMenuItem;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
-import javafx.scene.control.MenuBar;
+import javafx.scene.layout.TilePane;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Label;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
 
 public class CellSolutions extends Application
 {
+
+   Label outputLabel;
+   int blah;
    
    public static void main(String[] args)
    {
@@ -18,82 +24,41 @@ public class CellSolutions extends Application
    
    public void start(Stage stage)
    {
-      Menu planMenu = new Menu("Text");
+      // Stuff to Delete Later
+      blah = 0;
       
-      RadioMenuItem planA = new RadioMenuItem("Plan A");
-      RadioMenuItem planB = new RadioMenuItem("Plan B");
-      RadioMenuItem planC = new RadioMenuItem("Plan C");
+      // Plan Stuff
+      RadioButton planA = new RadioButton("Plan A");
+      RadioButton planB = new RadioButton("Plan B");
+      RadioButton planC = new RadioButton("Plan C");
       planA.setSelected(true);
-      
       ToggleGroup planToggleGroup = new ToggleGroup();
       planA.setToggleGroup(planToggleGroup);
       planB.setToggleGroup(planToggleGroup);
       planC.setToggleGroup(planToggleGroup);
+      planA.setOnAction(event -> calculateTotalBill());
+      planB.setOnAction(event -> calculateTotalBill());
+      planC.setOnAction(event -> calculateTotalBill());
       
-      // set actions for each button and plan
+      // Output Label Stuff
+      outputLabel = new Label("Output Label");
       
-      planMenu.getItems().add(planA);
-      planMenu.getItems().add(planB);
-      planMenu.getItems().add(planC);
+      // Adding Stuff to the Tile Pane
+      TilePane r = new TilePane();
+      r.getChildren().add(planA);
+      r.getChildren().add(planB);
+      r.getChildren().add(planC);
+      r.getChildren().add(outputLabel);
       
-      MenuBar mb = new MenuBar();
-      mb.getMenus().add(planMenu);
-      
-      VBox vb = new VBox(mb);
-      
-      Scene seePhoneBillScene = new Scene(vb, 500, 500);
-      
+      // Scene/Stage Stuff
+      Scene seePhoneBillScene = new Scene(r, 500, 500);
       stage.setScene(seePhoneBillScene);
-      
       stage.show();
    }
+   
+   private void calculateTotalBill()
+   {
+      blah = blah + 1;
+      outputLabel.setText("Yo!" + blah);
+   }
 }
-
-
-// import javafx.application.Application;
-// import javafx.stage.Stage;
-// import javafx.scene.Scene;
-// import javafx.scene.layout.Pane;
-// import javafx.scene.shape.Circle;
-// import javafx.util.Duration;
-// import javafx.animation.TranslateTransition;
-// 
-// public class JavaFXBallDrop extends Application
-// {
-//    public static void main(String[] args)
-//    {
-//       launch(args);
-//    }  // end main()
-//    
-//    public void start(Stage pStage)
-//    {
-//       // Constants for scene size
-//       final double SWIDTH = 200.0;
-//       final double SHEIGHT = 200.0;
-//       
-//       // Constants for the ball
-//       final double STARTX = 100.0, STARTY = 40.0;
-//       final double ENDX = 100.0, ENDY = 160.0;
-//       final double RADIUS = 40.0;
-//       // Constant for animation duration 
-//       final double ONESEC = 1000.0;
-//       
-//       // Create the ball
-//       Circle ball = new Circle(RADIUS);
-//       // Transition object
-//       TranslateTransition ttrans = new TranslateTransition(new Duration(ONESEC), ball);
-//       ttrans.setFromX(STARTX);
-//       ttrans.setFromY(STARTY);
-//       ttrans.setToX(ENDX);
-//       ttrans.setToY(ENDY);
-//       // Add the ball to the pane
-//       Pane pane = new Pane(ball);
-//       // Create the scene and display it
-//       Scene scene = new Scene(pane, SWIDTH, SHEIGHT);
-//       pStage.setScene(scene);
-//       pStage.setTitle("Animation of the Dropping Ball");
-//       pStage.show();
-//       // Play the transition
-//       ttrans.play();
-//    }  // end start()
-// }  // end class
